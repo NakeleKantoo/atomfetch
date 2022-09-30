1f366f629517763a21e67b9dc2eb5f05ef9d1aaa
Leonna <71112344+NakeleKantoo@users.noreply.github.com>
2022-09-29 21:43:50 -0300

    Done changing

---
+++ changeconfig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp config.txt /etc/atomfetch/
+echo "Configuration file changed!"
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-sudo mv ./atomfetch.lua /bin/atomfetch
-sudo chmod +x /bin/atomfetch
-sudo mkdir /etc/atomfetch
-sudo mv ./info.txt /etc/atomfetch
-echo "Done installing AtomFetch! Try it with a \"atomfetch -name Carbon\""
+#!/bin/bash
+sudo mkdir /etc/atomfetch/
+sudo mv info.txt /etc/atomfetch/
+sudo cp config.txt /etc/atomfetch/
+sudo chmod +x ./atomfetch
+sudo mv ./atomfetch /bin/atomfetch
+echo "Finished Installing AtomFetch, Try it with atomfetch --name carbon"
